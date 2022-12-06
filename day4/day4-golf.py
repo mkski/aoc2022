67c5526b890b9c472a9f34af3f9f075cdc89034a
Mike Kalaski <mikekalaski@gmail.com>
2022-12-06 11:36:21 -0500

    fix filename

--- day4/day4-golf.py
+++ day4/day4-golf.py
@@ -1,5 +1,5 @@
 c=v=0
-for l in open("i").read().split("\n"):
+for l in open("input").read().split("\n"):
     s,e,q,r=map(int,l.replace(",","-").split("-"))
     c+=(s>=q)&(e<=r)|(q>=s)&(r<=e)
     v+=(e>=q)&(r>=s)
