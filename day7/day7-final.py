4d217388a022f775b4b89941330f0fb4cc7bcd7f
Mike <mkalaski@flashpoint-intel.com>
2022-12-08 20:37:46 -0500

    more cleanup day 7

---
+++ day7/day7-final.py
@@ -0,0 +1,33 @@
+inputs = open("input").read().split("\n")
+
+def parse_cmd_lines(lines):
+    dir_sizes = {"/": 0}
+    for line in lines:
+        match line.split():
+            case "$", "cd", "/":
+                current_path = ["/"]
+            case "$", "cd", "..":
+                current_path.pop()
+            case "$", "cd", directory:
+                current_path.append(directory)
+            case "dir", directory:
+                continue
+            case "$", "ls":
+                continue
+            case size, _:
+                p = ""
+                for d in current_path:
+                    p += d
+                    if p in dir_sizes:
+                        dir_sizes[p] += int(size)
+                    else:
+                        dir_sizes[p] = int(size)
+    return dir_sizes
+
+
+dir_sizes = parse_cmd_lines(inputs)
+print(sum(s for s in dir_sizes.values() if s <= 100000))
+
+needed = 30000000 - (70000000 - dir_sizes["/"])
+candidates = [d for d in dir_sizes.values() if d >= needed]
+print(min(candidates))
