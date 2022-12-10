faf2c4d1f4e07f91aebcc70916a74d3ecf38a18a
Mike <mkalaski@flashpoint-intel.com>
2022-12-10 11:18:28 -0500

    day 10

---
+++ day10/day10.py
@@ -0,0 +1,34 @@
+program = open("input").read().split()
+
+
+def get_adds(p):
+    initial = [1]
+    for cycle in p:
+        match cycle:
+            case "noop" | "addx":
+                initial.append(0)
+            case value:
+                initial.append(int(value))
+    return initial
+
+
+states = get_adds(program)
+total = 0
+output = ""
+for cycle, _ in enumerate(states, 1):
+    x_register = sum(states[:cycle])
+    position = cycle % 40
+
+    if position == 20:
+        total += cycle * x_register
+
+    if position - 1 - x_register in (1, 0, -1):
+        output += "#"
+    else:
+        output += "."
+
+    if position == 0:
+        output += "\n"
+
+print(total)
+print(output)
