a02709e30cfcd4753b4b8c31a0a4cd2f8560ef67
Mike <mkalaski@flashpoint-intel.com>
2022-12-10 19:10:14 -0500

    simplify

--- day10/day10.py
+++ day10/day10.py
@@ -1,26 +1,14 @@
 program = open("input").read().split()
 
-
-def get_adds(p):
-    yield 1
-    for token in p:
-        yield 0 if token in ("addx", "noop") else int(token)
-
-
-adds = list(get_adds(program))
+adds = [1] + [0 if t in ("addx", "noop") else int(t) for t in program]
 total = 0
 output = ""
 for cycle, _ in enumerate(adds[:-1], 1):
     x_register = sum(adds[:cycle])
     position = cycle % 40
 
-    if position == 20:
-        total += cycle * x_register
-
-    if position - 1 - x_register in (1, 0, -1):
-        output += "#"
-    else:
-        output += "."
+    total += cycle * x_register if position == 20 else 0
+    output += "#" if position - 1 - x_register in (1, 0, -1) else '.'
 
     if position == 0:
         output += "\n"
